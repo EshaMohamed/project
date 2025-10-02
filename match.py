be01375f77b163a8e522b5486057cffaf5b0d554
EshaMohamed <eshamohamed247@gmail.com>
2025-10-02 12:38:56 +0530

    Create match.py

---
+++ match.py
@@ -0,0 +1,19 @@
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+op=input("enter the operator +,-,*,/:")
+
+match op:
+    case "+":
+        print("result:",num1+num2)
+    case "-":
+        print("result:",num1-num2)
+    case "*":
+        print("result:",num1*num2)
+    case "/":
+        if num2!=0:
+            print("result:",num1*num2)
+        else:
+           print("zero is not allowed")
+    case _:
+        print("default")
+
